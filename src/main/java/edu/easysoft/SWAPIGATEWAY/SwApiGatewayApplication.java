a3fd00ac47d69c89827d823e9d2548edd0a64c8a
unknown <alexandrscherbakov231194@gmail.com>
2023-02-10 18:21:30 +0200

    initial commit

---
+++ src/main/java/edu/easysoft/SWAPIGATEWAY/SwApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package edu.easysoft.SWAPIGATEWAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/easysoft/SWAPIGATEWAY/SwApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.easysoft.SWAPIGATEWAY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
